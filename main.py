3106f82171ad87b5562bfd90954b12daee4f6857
Nennneko5787 <nennneko5787@gmail.com>
2024-11-03 18:52:40 +0900

    send_money

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ async def setup_hook():
     await bot.load_extension("cogs.link")
     await bot.load_extension("cogs.jihanki_edit")
     await bot.load_extension("cogs.jihanki_panel")
+    await bot.load_extension("cogs.send_money")
 
 
 @asynccontextmanager
